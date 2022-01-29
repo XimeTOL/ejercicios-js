d51b7815ef8ef17a05f6a15825096d4be24200d6
Xime Hackademy <ada_wit25@hackademy.mx>
2022-01-28 23:12:33 -0600

    AGREGUE LOS ARCHIVOS
    Ximena Toledo

--- script.js
+++ script.js
@@ -2,36 +2,56 @@ const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
-
-const ejercicio2 = [
+let primo = val => {
+  for(let i = 2; i < val; i++)
   {
-    nombre: "Gabriel",
+    if(val % i == 0)
+    return "no";
+  }
+  return "sí";
+}
+ejercicio1.forEach(val => {
+  console.log(`El valor ${val} , ${primo(val)} es primo  `)
+});
+
+const amigo1 = {
+    nombre: "Moisés",
     edad: 22,
     esFamiliar: false,
-  },
-  {
-    nombre: "Jaime",
+  };
+const amigo2 = {
+    nombre: "Sebas",
     edad: 15,
     esFamiliar: true,
-  },
-  {
-    nombre: "María",
+  };
+  const amigo3 =   {
+    nombre: "Dan",
     edad: 18,
     esFamiliar: true,
-  },
-  {
-    nombre: "Angel",
+  };
+  const amigo4 =   {
+    nombre: "Iñaki",
     edad: 19,
     esFamiliar: true,
-  },
-  {
-    nombre: "Fer",
+  };
+  const amigo5 =   {
+    nombre: "Mike",
     edad: 18,
     esFamiliar: true,
-  },
-  {
-    nombre: "Rachel",
+  };
+  const amigo6 =   {
+    nombre: "Armando",
     edad: 30,
     esFamiliar: true,
-  },
-];
+  };
+const Persona = [amigo1,amigo2,amigo3,amigo4,amigo5,amigo6];
+const AdmitirPaso = (persona) => {
+    if(persona.edad >= 18 && persona.esFamiliar == true ){
+        console.log(`Puedes pasar ${persona.nombre} con ${persona.edad} años`);
+    }
+    else
+    {
+        console.log(`No puedes pasar ${persona.nombre} con ${persona.edad} años`);
+    }
+};
+Persona.forEach((persona => AdmitirPaso(persona)));
